df21a81b547b4d361552255abcfac67c05de83bc
ricardo99 <ricardo99@Ricardo>
2019-02-25 18:00:55 +0100

    commit 3. Magos getters and setters

--- Casa02/src/fate/Mago.java
+++ Casa02/src/fate/Mago.java
@@ -5,4 +5,29 @@ public class Mago {
 	protected String clase;
 	protected String idClase;
 	protected Sirviente servant;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getClase() {
+		return clase;
+	}
+	public void setClase(String clase) {
+		this.clase = clase;
+	}
+	public String getIdClase() {
+		return idClase;
+	}
+	public void setIdClase(String idClase) {
+		this.idClase = idClase;
+	}
+	public Sirviente getServant() {
+		return servant;
+	}
+	public void setServant(Sirviente servant) {
+		this.servant = servant;
+	}
+	
 }
